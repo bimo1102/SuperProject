aed7df594e311ad5a628396b3f6e66c5f46e663f
bimo1102 <levanlam270920@gmail.com>
2025-06-14 10:26:28 +0700

    update code

--- backend/src/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ backend/src/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+362b01b8e2b0c65735ab7175eb432d39f81f2512")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b1968a47b3e41bbf9007c489773fee31290684")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
